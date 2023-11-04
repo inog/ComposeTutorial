e84b27b3f383f41f5596a889c4494e029ef4caf7
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-04 21:32:25 +0100

    make crazy things with modifiers

--- app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
+++ app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,8 +49,11 @@ fun MessageCard(msg: MyMessage) {
             painter = painterResource(R.drawable.profile_picture),
             contentDescription = "Contact profile picture",
             modifier = Modifier
-                .height(40.dp)
-                .width(40.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.dp, MaterialTheme.colorScheme.primary, CircleShape)
+
+
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
@@ -59,9 +67,13 @@ fun MessageCard(msg: MyMessage) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = MyMessage("Ingo", "Have fun with that stuff")
-    )
+    ComposeTutorialTheme {
+        Surface (color = MaterialTheme.colorScheme.background) {
+            MessageCard(
+                msg = MyMessage("Ingo", "Have fun with that stuff")
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
