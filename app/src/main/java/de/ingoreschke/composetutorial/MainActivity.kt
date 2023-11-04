7d81efe0fb31c1de2fd097cde7fd5afdc6ab7059
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-04 21:18:54 +0100

    add a row element and a picture

--- app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
+++ app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
@@ -1,17 +1,23 @@
 package de.ingoreschke.composetutorial
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.ingoreschke.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,19 +34,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class MyMessage(val author:String, val body:String);
+data class MyMessage(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: MyMessage) {
-    Column() {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture"
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
+fun PreviewMessageCard() {
     MessageCard(
         msg = MyMessage("Ingo", "Have fun with that stuff")
     )
