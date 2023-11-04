015ba6122efc1cb7b45fd699c5b18403572de54d
Ingo Reschke <kontakt@ingo-reschke.de>
2023-11-04 21:49:29 +0100

    add shape

--- app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
+++ app/src/main/java/de/ingoreschke/composetutorial/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.ingoreschke.composetutorial.ui.theme.ComposeTutorialTheme
+import de.ingoreschke.composetutorial.ui.theme.shapeScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +45,7 @@ data class MyMessage(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: MyMessage) {
-    Row (modifier = Modifier.padding(all = 8.dp)) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
             contentDescription = "Contact profile picture",
@@ -52,14 +53,23 @@ fun MessageCard(msg: MyMessage) {
                 .size(40.dp)
                 .clip(CircleShape)
                 .border(1.dp, MaterialTheme.colorScheme.primary, CircleShape)
-
-
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface (shape = MaterialTheme.shapeScheme.large , shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(end = 8.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
@@ -68,7 +78,7 @@ fun MessageCard(msg: MyMessage) {
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        Surface (color = MaterialTheme.colorScheme.background) {
+        Surface(color = MaterialTheme.colorScheme.background) {
             MessageCard(
                 msg = MyMessage("Ingo", "Have fun with that stuff")
             )
